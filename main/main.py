50ba05c35676f1c96bcbcc9e0edbac5505b88f1a
huangyin0611 <1071324110@qq.com>
2019-01-14 15:51:36 +0800

    convolution forward and pooling forward

--- main/main.py
+++ main/main.py
@@ -28,4 +28,23 @@ if __name__ == '__main__':
     axarr[1].set_title('x_pad')
     axarr[1].imshow(x_pad[0, :, :, 0])
 
-    plt.show()
+    # plt.show()
+
+    # ----------convolution multiply
+    np.random.seed(1)
+    a_slice_prev = np.random.randn(4, 4, 3)
+    W = np.random.randn(4, 4, 3)
+    b = np.random.randn(1, 1, 1)
+    Z = conv_single_step(a_slice_prev, W, b)
+    print("Z = ", Z)
+
+    # ----------convolution forward
+    np.random.seed(1)
+    A_prev = np.random.randn(10, 4, 4, 3)
+    W = np.random.randn(2, 2, 3, 8)
+    b = np.random.randn(1, 1, 1, 8)
+    hparameters = {"pad": 2,
+                   "stride": 1}
+    Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
+    print("Z's mean = ", np.mean(Z))
+    print("cache_conv[0][1][2][3] = ", cache_conv[0][1][2][3])
