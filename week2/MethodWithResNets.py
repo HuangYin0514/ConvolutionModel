5b538ec2e8b998513ca9f0549119a8a726ab5299
huangyin0611 <1071324110@qq.com>
2019-01-18 16:21:15 +0800

    ResNet 50

--- week2/MethodWithResNets.py
+++ week2/MethodWithResNets.py
@@ -127,3 +127,64 @@ def convolutional_block(X, f, filters, stage, block, s=2):
     X = Activation('relu')(X)
 
     return X
+
+
+def ResNet50(input_shape=(64, 64, 3), classes=6):
+    """
+       Implementation of the popular ResNet50 the following architecture:
+       CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+       -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+
+       Arguments:
+       input_shape -- shape of the images of the dataset
+       classes -- integer, number of classes
+
+       Returns:
+       model -- a Model() instance in Keras
+       """
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # ZeroPadding2D
+    X = ZeroPadding2D((3, 3))(X_input)
+
+    # Stage1
+    X = Conv2D(64, (7, 7), strides=(2, 2), name="conv1", kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name="bn_conv1")(X)
+    X = Activation('relu')(X)
+    X = MaxPooling2D((3, 3), strides=(2, 2))(X)
+
+    # Stage2
+    X = convolutional_block(X, f=3, filters=[64, 64, 256], stage=2, block='a', s=1)
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='b')
+    X = identity_block(X, 3, [64, 64, 256], stage=2, block='c')
+
+    # Stage3
+    X = convolutional_block(X, f=3, filters=[128, 128, 256], stage=3, block='a', s=2)
+    X = identity_block(X, 3, [128, 128, 256], stage=3, block='b')
+    X = identity_block(X, 3, [128, 128, 256], stage=3, block='c')
+    X = identity_block(X, 3, [128, 128, 256], stage=3, block='d')
+
+    # Stage4
+    X = convolutional_block(X, f=3, filters=[256, 256, 1024], stage=4, block='a', s=2)
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='b')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='c')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='d')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='e')
+    X = identity_block(X, 3, [256, 256, 1024], stage=4, block='f')
+
+    # Stage5
+    X = convolutional_block(X, f=3, filters=[512, 512, 2048], stage=5, block='a', s=2)
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='b')
+    X = identity_block(X, 3, [512, 512, 2048], stage=5, block='c')
+
+    # AveragePooling
+    X = AveragePooling2D(pool_size=(2, 2), padding='same')(X)
+
+    # output layer
+    X = Flatten()(X)
+    X = Dense(classes, activation='softmax', name='fc' + str(classes),
+              kernel_initializer=glorot_uniform(seed=0))(X)
+
+    model = Model(inputs=X_input, outputs=X, name="ResNet50")
+    return model
